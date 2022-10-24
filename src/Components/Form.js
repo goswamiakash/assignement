6b945f6e239a6d15d6a3e1fab400b1798fe7824b
AshifAli007 <theashifkhan@gmail.com>
2022-10-24 16:12:22 +0530

    lower section fixed

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -24,7 +24,9 @@ const Form = () => {
             padding: "1% 2% 0 6%",
           }}
         >
-          <h2 style={{ textAlign: "left", paddingLeft: '3%' }}>Personal Information:</h2>
+          <h2 style={{ textAlign: "left", paddingLeft: "3%" }}>
+            Personal Information:
+          </h2>
 
           <div style={{ paddingLeft: "15%" }}>
             <div>
@@ -48,9 +50,13 @@ const Form = () => {
             <div>
               <p className="para">UID:</p>
               <input className="form-input" type="text" name="" value="" />
-              <p className="para" style={{ marginRight: "10px", marginLeft: "20%" }}>Email:</p>
+              <p
+                className="para"
+                style={{ marginRight: "10px", marginLeft: "20%" }}
+              >
+                Email:
+              </p>
               <input className="form-input" type="text" name="" value="" />
-
             </div>
             <div>
               <p className="para">Address 1:</p>
@@ -71,7 +77,10 @@ const Form = () => {
               <input className="form-input" type="text" name="" value="" />
               {/* </div>
           <div> */}
-              <p className="para" style={{ marginRight: "10px", marginLeft: "20%" }}>
+              <p
+                className="para"
+                style={{ marginRight: "10px", marginLeft: "20%" }}
+              >
                 State:
               </p>
               <input className="form-input" type="text" name="" value="" />
@@ -85,10 +94,12 @@ const Form = () => {
           <hr />
 
           <div className="horizontal-2">
-            <h2 style={{ textAlign: "left", paddingLeft: '3%' }}>Research Information:</h2>
+            <h2 style={{ textAlign: "left", paddingLeft: "3%" }}>
+              Research Information:
+            </h2>
 
             <div style={{ paddingLeft: "15%" }}>
-              <div >
+              <div>
                 <p className="para">Subject of your research: </p>
                 <textarea
                   className="form-input"
@@ -131,12 +142,12 @@ const Form = () => {
                 marginBottom: "0",
                 paddingTop: "0",
                 margin: "0",
-                paddingLeft: '3%'
+                paddingLeft: "3%",
               }}
             >
               Expense Information:
             </h2>
-            <p style={{ paddingLeft: '3%' }}>
+            <p style={{ paddingLeft: "3%" }}>
               (Transportation, Lodging, Conference,Registration Only - Actual or
               Estimated)
             </p>
@@ -170,8 +181,8 @@ const Form = () => {
                 <textarea
                   className="form-input"
                   type="textarea"
-                  rows='5'
-                  cols = '20'
+                  rows="5"
+                  cols="20"
                   name=""
                   value=""
                 />
@@ -185,125 +196,131 @@ const Form = () => {
           <hr />
 
           <div className="horizontal-4">
-            <h2 style={{ textAlign: "left",paddingLeft:'3%' }}>Additional Information:</h2>
-            <div style={{paddingLeft:'15%'}}>
-            <div style={{ display: "flex", flexFlow: "row wrap"}}>
-              <p className="para">
-                I have applied to another organization fro funding
-              </p>
-              <div
-                style={{
-                  display: "flex",
-                  flexFlow: "column wrap",
-                  justifyContent: "space-evenly",
-                }}
-              >
-                <div style={{ display: "flex", flexFlow: "row wrap" }}>
-                  <input
-                    id="funding-yes"
-                    name="funding"
-                    type="radio"
-                    value="Yes"
-                  />
-                  <label htmlFor="funding-yes">Yes</label>
-                </div>
+            <h2 style={{ textAlign: "left", paddingLeft: "3%" }}>
+              Additional Information:
+            </h2>
+            <div style={{ paddingLeft: "15%" }}>
+              <div style={{ display: "flex", flexFlow: "row wrap" }}>
+                <p className="para">
+                  I have applied to another organization fro funding
+                </p>
+                <div
+                  style={{
+                    display: "flex",
+                    flexFlow: "column wrap",
+                    justifyContent: "space-evenly",
+                  }}
+                >
+                  <div style={{ display: "flex", flexFlow: "row wrap" }}>
+                    <input
+                      id="funding-yes"
+                      name="funding"
+                      type="radio"
+                      value="Yes"
+                    />
+                    <label htmlFor="funding-yes">Yes</label>
+                  </div>
 
-                <div style={{ display: "flex", flexFlow: "row wrap" }}>
-                  <input
-                    id="funding-no"
-                    name="funding"
-                    type="radio"
-                    value="No"
-                  />
-                  <label htmlFor="funding-no">No</label>
-                </div>
-              </div>
-            </div>
-            <div>
-              <p className="para">Name of Organiastion</p>
-              <input className="form-input" type="text" value="" />
-            </div>
-            <div>
-              <p className="para">
-                Funding received?
-              </p>
-              <div
-                style={{
-                  display: "flex",
-                  flexFlow: "column wrap",
-                  justifyContent: "space-evenly",
-                }}
-              >
-                <div style={{ display: "flex", flexFlow: "row wrap" }}>
-                  <input
-                    id="funding-yes"
-                    name="funding"
-                    type="radio"
-                    className="form-input"
-                    value="Yes"
-                  />
-                  <label htmlFor="funding-yes">Yes</label>
-                </div>
-                <div style={{ display: "flex", flexFlow: "row wrap" }}>
-                  <input
-                    id="funding-no"
-                    name="funding"
-                    className="form-input"
-                    type="radio"
-                    value="No"
-                  />
-                  <label htmlFor="funding-no">No</label>
-                </div>
-                <div style={{ display: 'flex', flexflow: 'row wrap' }}>
-                  <input
-                    id="funding-no"
-                    name="funding"
-                    className="form-input"
-                    type="radio"
-                    value="No"
-                  />
-                  <label htmlFor="funding-no">Don't know yet</label>
-                </div>
-                <div>
-                  <p className="para">
-                    Other amount received:
-                  </p >
-                  <div>
-                    <input className="form-input" type="text" name="" value='' />
+                  <div style={{ display: "flex", flexFlow: "row wrap" }}>
+                    <input
+                      id="funding-no"
+                      name="funding"
+                      type="radio"
+                      value="No"
+                    />
+                    <label htmlFor="funding-no">No</label>
                   </div>
                 </div>
-                <div>
-                  <p className="para">Note: hhcsjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</p>
-                </div>
-                <div>
-                  <p className="para">
-                    Additional comments:
-                  </p>
-                  <div>
-                    <textarea  className="form-input" name="" id="" cols="30" rows="5"></textarea>
+              </div>
+              <div>
+                <p className="para">Name of Organiastion</p>
+                <input className="form-input" type="text" value="" />
+              </div>
+              <div style={{ display: "flex", flexFlow: "row wrap" }}>
+                <p className="para">Funding received?</p>
+                <div
+                  style={{
+                    display: "flex",
+                    flexFlow: "column wrap",
+                    justifyContent: "space-evenly",
+                  }}
+                >
+                  <div style={{ display: "flex", flexFlow: "row wrap" }}>
+                    <input
+                      id="funding-yes"
+                      name="funding"
+                      type="radio"
+                      className="form-input"
+                      value="Yes"
+                    />
+                    <label htmlFor="funding-yes">Yes</label>
+                  </div>
+                  <div style={{ display: "flex", flexFlow: "row wrap" }}>
+                    <input
+                      id="funding-no"
+                      name="funding"
+                      className="form-input"
+                      type="radio"
+                      value="No"
+                    />
+                    <label htmlFor="funding-no">No</label>
+                  </div>
+                  <div style={{ display: "flex", flexflow: "row wrap" }}>
+                    <input
+                      id="funding-no"
+                      name="funding"
+                      className="form-input"
+                      type="radio"
+                      value="No"
+                    />
+                    <label htmlFor="funding-no">Don't know yet</label>
                   </div>
                 </div>
               </div>
 
-            </div>
+              <div style={{ display: "flex", flexFlow: "row wrap" }}>
+                <p className="para">Other amount received</p>
 
-            </div>
+                <input className="form-input" type="text" name="" value="" />
+              </div>
+              <div>
+                <p className="para">
+                  Note:
+                  hhcsjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+                </p>
+              </div>
+              <div style={{ display: "flex", flexFlow: "row wrap" }}>
+                <p className="para">Additional comments</p>
 
-
-          </div>
-
-          <div className="footer" style={{ paddingTop: '10px', paddingLeft: '40px', paddingTop: '10px' }}>
-            <div >
-              <button className="btn">Submit</button>
-            </div>
-            <div>
-              <button className="btn">Reset</button>
-            </div>
-            <div>
-              <button className="btn">Back</button>
+                <textarea
+                  className="form-input"
+                  name=""
+                  id=""
+                  cols="30"
+                  rows="5"
+                ></textarea>
+              </div>
             </div>
           </div>
         </form>
+        <div
+          className="footer"
+          style={{
+            paddingTop: "10px",
+            paddingLeft: "40px",
+            paddingTop: "10px",
+          }}
+        >
+          <div>
+            <button className="btn">Submit</button>
+          </div>
+          <div>
+            <button className="btn">Reset</button>
+          </div>
+          <div>
+            <button className="btn">Back</button>
+          </div>
+        </div>
       </div>
     </div>
   );
